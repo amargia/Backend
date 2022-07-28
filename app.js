ff56a199379d605b435f949b5164acac3e71b418
amargia <amarzuqa@gmail.com>
2022-07-27 22:01:17 -0400

    templates

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
 const router = require('./routes/index');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', router);
+
+app.set("view engine", "ejs");
+
+const data = []
+
+app.get("/", (req, res) => {
+  res.render("datosEJS", { data })
+})
+
+app.post("/products", (req, res) => {
+  data.push(req.body);
+  res.render("datosEJS", { data })
+});
+
 
 let PORT = 8080
 const server = app.listen(PORT,()=>{
@@ -9,7 +28,3 @@ const server = app.listen(PORT,()=>{
 
 server.on("error",error=>`El servidor a tenido un error:${error}`);
 app.use(express.static('public'));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api', router);
