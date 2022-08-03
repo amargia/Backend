6fd719fb23f99f078212186efeef9fecc5827c52
amargia <amarzuqa@gmail.com>
2022-08-03 15:21:03 -0400

    Templates

--- HBS/index.js
+++ HBS/index.js
@@ -1,21 +1,32 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const handlebars = require("express-handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.engine(
   "hbs",
   handlebars.engine({
     extname: ".hbs",
-    defaultLayout: "index.hbs",
+    defaultLayout: "datos.hbs",
     layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
   })
 );
 
-app.set("view engine", "hbs");
-app.set("views", "./views");
+const data = []
 
-app.get("/", function (req, res) {
-    const { title, price, thumbnail } = req.query;
-    res.render("main", { title, price, thumbnail });
+app.set("view engine", "hbs");
+app.set("views", "../HBS/views");
+
+app.get("/", (req, res) => {
+  res.render("main", { data })
+})
+
+app.post("/datos", (req, res) => {
+  data.push(req.body);
+  res.render("main", { data })
 });
 
 app.listen(8080, () => console.log("Server up"));
